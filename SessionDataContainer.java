e0df12c2bfe60437b401f37da862608868c15c4c
akszone <prakashmsharma@gmail.com>
2017-04-24 20:49:58 -0400

    Test Commit

---
+++ SessionDataContainer.java
@@ -0,0 +1,20 @@
+public void SessionDataContainer{
+private HashMap<String, String> dataContainer=new HashMap();
+
+//This need to be a singleton Class
+
+void put(String Key,V value){
+this.dataContainer.put(key,value);
+return void;
+}
+
+public String remove(String k){
+return dataContainer!=null dataContainer.get(k)
+}
+
+public String removeAll(){
+}
+}
+
+public class MutableHttpServletRequest{
+}
